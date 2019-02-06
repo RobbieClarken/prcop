99622d49539b77dd1ebae58d66d72cce261b4180
RobbieClarken <robbie.clarken@gmail.com>
2019-02-06 21:36:16 +1100

    Extract Repo and PullRequest classes

--- src/prcop/checker.py
+++ src/prcop/checker.py
@@ -7,6 +7,76 @@ import requests
 from .business_hours import within_business_hours
 
 
+class PullRequest:
+
+    _MIN_TIME_OPENED = timedelta(hours=3)
+    _MIN_APPROVALS = 2
+
+    def __init__(self, data, *, repo, record):
+        self._data = data
+        self._repo = repo
+        self._record = record
+
+    def alerts(self):
+        if (
+            self._time_since_opened >= self._MIN_TIME_OPENED
+            and not self._recently_alerted
+            and self._approvals < self._MIN_APPROVALS
+            and not self._needs_work
+        ):
+            self._record.record_alert(self._id)
+            return [self._alert_message]
+        return []
+
+    @property
+    def _id(self):
+        return str(self._data["id"])
+
+    @property
+    def _title(self):
+        return self._data["title"]
+
+    @property
+    def _recently_alerted(self):
+        return self._record.alerted_recently(self._id)
+
+    @property
+    def _approvals(self):
+        return sum(review["status"] == "APPROVED" for review in self._data["reviewers"])
+
+    @property
+    def _needs_work(self):
+        return any(review["status"] == "NEEDS_WORK" for review in self._data["reviewers"])
+
+    @property
+    def _time_since_opened(self):
+        return datetime.now() - datetime.fromtimestamp(self._data["createdDate"] / 1000)
+
+    @property
+    def _alert_message(self):
+        return f'{self._repo.project_slug}/{self._repo.slug} PR: "{self._title}" needs reviews'
+
+
+class Repo:
+    def __init__(self, base_url, project, repo, *, record):
+        self._base_url = base_url
+        self.project_slug = project
+        self.slug = repo
+        self._record = record
+
+    def alerts(self):
+        url = (
+            f"{self._base_url}/rest/api/1.0/projects/{self.project_slug}"
+            f"/repos/{self.slug}/pull-requests"
+        )
+        response = requests.get(url)
+        alerts = []
+        for pr_data in response.json()["values"]:
+            pr = PullRequest(pr_data, repo=self, record=self._record)
+            alerts += pr.alerts()
+        return alerts
+
+
 class Checker:
     def __init__(self, *, record):
         self._record = record
@@ -14,21 +84,7 @@ class Checker:
     def check(self, base_url, project, repo):
         if not within_business_hours(datetime.now()):
             return []
-        url = f"{base_url}/rest/api/1.0/projects/{project}/repos/{repo}/pull-requests"
-        response = requests.get(url)
-        alerts = []
-        for pr in response.json()["values"]:
-            pr_id = str(pr["id"])
-            opened = datetime.fromtimestamp(pr["createdDate"] / 1000)
-            if datetime.now() - opened < timedelta(hours=3):
-                continue
-            approvals = sum(review["status"] == "APPROVED" for review in pr["reviewers"])
-            needs_work = any(review["status"] == "NEEDS_WORK" for review in pr["reviewers"])
-            if not self._record.alerted_recently(pr_id) and approvals < 2 and not needs_work:
-                title = pr["title"]
-                alerts.append(f'{project}/{repo} PR: "{title}" needs reviews')
-                self._record.record_alert(pr_id)
-        return alerts
+        return Repo(base_url, project, repo, record=self._record).alerts()
 
 
 class JsonRecord:
